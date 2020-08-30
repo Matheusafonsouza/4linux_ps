dbe262ca3a81b0d1a47335547f963b95d4c337e7
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-30 20:14:21 -0300

    feature: Created home page

---
+++ web/src/components/Card/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Card() {
+  return (
+
+    <div className="api">
+      <div className="left-api">
+        <h1>Cat Facts / Animals</h1>
+        <span className="description" >auheuaheuhaueuahuehauhueahuehu</span>
+        <span className="link">https://alexwohlbruck.github.io/cat-facts/</span>
+      </div>
+      <div className="right-api">
+        <span>https: true</span>
+        <span>cors: no</span>
+        <span>auth: não sei</span>
+      </div>
+    </div>
+  );
+}
--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { FiLogOut } from 'react-icons/fi';
+
+import Card from '../../components/Card';
+
+import './styles.css';
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="home">
+      <div className="left-bar">
+        <span>
+          <FiLogOut />
+          logout
+        </span>
+
+        <h1>Número de API's: 123</h1>
+
+        <div className="input-group">
+          <input type="text" placeholder="Categoria" />
+          <input type="text" placeholder="Nome" />
+        </div>
+      </div>
+      <div className="right-bar">
+        <div className="api-grid">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
   );
 }
