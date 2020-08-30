70e241ade7ac87d1570d96f59aab7acd39d89d71
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-30 19:10:08 -0300

    feature: Created login page

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Routes from './Routes';
 
+import './global.css';
+
 function App() {
   return (
     <Routes />
--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import './styles.css';
+
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <div className="home">
+      <div className="left-bar">
+        <h1>Olá!</h1>
+        <span>Você ainda não tem uma conta?</span>
+        <button type="button">Criar conta</button>
+      </div>
+      <div className="right-bar">
+        <h1>Logar</h1>
+        <form>
+          <div className="input-group">
+            <input type="text" />
+            <input type="text" />
+          </div>
+          <button>Entrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
