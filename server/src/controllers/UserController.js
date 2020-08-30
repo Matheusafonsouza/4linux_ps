9abde48bedbad52b4b153a83ab02c6641a355914
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-30 18:27:57 -0300

    feature: created login method with jwt

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,10 +1,47 @@
-const uuid = require('uuidv4');
-const connection = require('../database/index');
 const bcrypt = require('bcryptjs');
+const uuid = require('uuidv4');
+const jwt = require('jsonwebtoken');
+
+const connection = require('../database/index');
+const authConfig = require('../config/auth');
 
 module.exports = {
+  async login(request, response) {
+    const { name, password } = request.body;
+
+    const user = await connection('users').where('name', name).first();
+
+    if (!user) {
+      return response.status(400).json({ error: 'User does not exists.' });
+    }
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) {
+      return response.status(401).json({ error: 'User not allowed.' })
+    }
+
+    const { expiresIn, secret } = authConfig.jwt;
+
+    const token = jwt.sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    delete user.password;
+
+    return response.status(200).json({ user, token });
+
+  },
+
   async store(request, response) {
     const { name, password } = request.body;
+
+    const checkUserExists = await connection('users').where('name', name).first();
+
+    if (checkUserExists) {
+      return response.status(400).json({ error: 'User already exists.' });
+    }
 
     const hashedPassword = await bcrypt.hash(password, 8);
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,7 @@ const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
-routes.post('/login', UserController.store);
+routes.post('/signup', UserController.store);
+routes.post('/login', UserController.login);
 
 module.exports = routes;
